8dcc97d81ee2cdf657c13d0aa1c3e0a0932def2e
Mélusine <melusine.hermier@proton.me>
2024-12-01 18:25:23 +0100

    be explicit in slice size

--- v1/integrity.go
+++ v1/integrity.go
@@ -26,7 +26,7 @@ func NewIntegrity(key []byte, id uuid.UUID, data []byte) (*Integrity, error) {
 	if len(key) < KEY_EXTRACT_SIZE { // should be redundant, key should be 32 bytes
 		return nil, types.NewBadKeyErr(fmt.Sprintf("expected len >= %d, got %d", KEY_EXTRACT_SIZE, len(key)))
 	}
-	versionBin := []byte{}
+	versionBin := make([]byte, 4)
 	binary.LittleEndian.PutUint64(versionBin, uint64(VERSION)) // force into an uint64: uint might be 32 bits or 64 bits depending on platform
 	uuidBin, err := id.MarshalBinary()
 	if err != nil {
