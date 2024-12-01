bfc727f5cfc08206c960db6bbdb7155821da7679
Mélusine <melusine.hermier@proton.me>
2024-12-01 16:22:45 +0100

    test encryption with an invalid algorithm

---
+++ rec_test.go
@@ -0,0 +1,32 @@
+package rec
+
+import (
+	"testing"
+
+	"github.com/melsincostan/rec/types"
+)
+
+const nonexistentVersion = uint(0)
+
+type testStruct struct {
+	String string
+}
+
+var testStructInstance = testStruct{
+	String: "test",
+}
+var key [32]byte // should all already be 0
+
+func TestEncrypt(t *testing.T) {
+	// test that a non-existent version returns an error
+	res, err := Encrypt(nonexistentVersion, key[:], testStructInstance)
+	if err == nil {
+		t.Error("expected error, got none")
+	} else if _, ok := err.(*types.ErrUnimplemented); !ok {
+		t.Errorf("expected error of type types.ErrUnimplemented, got %#v", err)
+	}
+
+	if res != nil {
+		t.Errorf("expected nil result, got %#v", res)
+	}
+}
