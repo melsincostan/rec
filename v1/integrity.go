9971f35f018d887d8544da45c27074b61abeaf3f
Mélusine <melusine.hermier@proton.me>
2024-12-01 15:27:54 +0100

    add documentation for the integrity part of v1

--- v1/integrity.go
+++ v1/integrity.go
@@ -9,8 +9,11 @@ import (
 	"github.com/melsincostan/rec/types"
 )
 
+// KEY_EXTRACT_SIZE defined the number of bytes from the key that should be used as part of the integrity check
 const KEY_EXTRACT_SIZE = int(4)
 
+// Integrity holds the various components used to compute a SHA256 hash to fingerprint a record.
+// It also offers a Digest function to generate said hash.
 type Integrity struct {
 	KeyExtract []byte // first 4 bytes of the key
 	ID         []byte
@@ -18,6 +21,7 @@ type Integrity struct {
 	Data       []byte // full data, it's going to be hashed anyways
 }
 
+// NewIntegrity returns an Integrity struct with the internal fields filled using the provided informations.
 func NewIntegrity(key []byte, id uuid.UUID, data []byte) (*Integrity, error) {
 	if len(key) < KEY_EXTRACT_SIZE { // should be redundant, key should be 32 bytes
 		return nil, types.NewBadKeyErr(fmt.Sprintf("expected len >= %d, got %d", KEY_EXTRACT_SIZE, len(key)))
@@ -36,11 +40,14 @@ func NewIntegrity(key []byte, id uuid.UUID, data []byte) (*Integrity, error) {
 	}, nil
 }
 
+// Digest returns a byte array holding the SHA256 digest of the binary representation of the information.
+// This binary representation is obtained using Bin().
 func (i Integrity) Digest() []byte {
 	digest := sha256.Sum256(i.Bin())
 	return digest[:]
 }
 
+// Bin returns a representation of all of the information as a single byte array.
 func (i Integrity) Bin() []byte {
 	res := []byte{}
 	res = append(res, i.KeyExtract...)
