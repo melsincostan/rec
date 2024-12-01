28f1360dd23d5aa9c383999cc07fe1b4ecd65807
Mélusine <melusine.hermier@proton.me>
2024-12-01 15:42:02 +0100

    add documentation for V1 operations

--- v1/operations.go
+++ v1/operations.go
@@ -12,9 +12,22 @@ import (
 	"github.com/melsincostan/rec/types"
 )
 
+// VERSION defined the version uint number of these implementations.
+// Since this is used to compute integrity, changing this will render existing records unusable.
+// This shouldn't be changed anyways.
+// A new version should be implemented instead, and records can be migrated to it using the Rotate function.
 const VERSION = uint(1)
+
+// KEY_SIZE expresses the number of bytes in a key.
+// Since the plan is to use AES256, the key must be 32 bytes (32 * 8 = 256).
 const KEY_SIZE = int(32)
 
+// Encrypt takes a key and data in an arbitrary format, and returns an EnryptedRecord or an error.
+// It uses gob to turn the data into a binary representation.
+// This binary is then encrypted using AES256 with the provided key.
+// For the ID, an uuid is generated.
+// This also sets an integrity hash which bases on the id, the data itself, the version and the encryption key.
+// Note: since gob is used under the hood, one might have to use gob.Register to avoid errors with some custom types.
 func Encrypt(key []byte, data any) (*types.EncryptedRecord, error) {
 	if len(key) != KEY_SIZE { // key size for AES256
 		return nil, types.NewBadKeyErr(fmt.Sprintf("key size must be %d, got %d", KEY_SIZE, len(key)))
@@ -57,6 +70,8 @@ func Encrypt(key []byte, data any) (*types.EncryptedRecord, error) {
 	}, nil
 }
 
+// Decrypt takes an EncryptedRecord and, provided the integrity checksum matches with the provided data, tries to decrypt it.
+// The generic type should be specified, as it cannot be inferred from the EncryptedRecord.
 func Decrypt[T any](key []byte, data types.EncryptedRecord) (*T, error) {
 	if len(key) != KEY_SIZE { // key_size for AES256
 		return nil, types.NewBadKeyErr(fmt.Sprintf("key size must be %d, got %d", KEY_SIZE, len(key)))
